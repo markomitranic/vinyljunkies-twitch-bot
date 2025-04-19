31bc3361ba2c3b3b677f59a3e592c5c29e717eb7
Marko Mitranic <marko.mitranic@gmail.com>
2025-04-19 17:05:58 +0200

    Enhance Twitch OAuth token authorization UI

--- src/auth.ts
+++ src/auth.ts
@@ -11,16 +11,59 @@ app.get("/", (_req, res) => {
     webpage(
       "Twitch Bot Auth",
       `
-        <h1 class="text-3xl font-bold underline text-clifford">Hello World!</h1>
-        <p>
-          We use the <a href="https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#authorization-code-grant-flow" target="_blank">Authorization code grant flow</a> to authenticate with Twitch.
-          <br>
-          In order to do this, please provide the following credentials:
-        </p>
-        <form action="/authorize">
-          <input type="text" name="client_id" value="${process.env.APP_CLIENT_ID}" />
-          <button type="submit">Authorize on Twitch</button>
-        </form>
+        <div class="flex flex-col gap-2 items-start justify-start">
+          <h1 class="text-3xl font-bold underline text-clifford mb-6">Create a Twitch OAuth Token!</h1>
+          <p>
+            We use the
+            <a
+              href="https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#authorization-code-grant-flow"
+              target="_blank"
+              >Authorization code grant flow</a
+            >
+            to authenticate with Twitch.
+            <br />
+            In order to do this, please provide the following credentials:
+          </p>
+          <form action="/authorize" class="flex flex-col gap-2 items-start justify-start">
+            <label
+              for="client_id"
+              class="block text-sm/6 font-medium text-gray-900"
+              >App Client ID</label
+            >
+            <input
+              type="text"
+              name="client_id"
+              value="${process.env.APP_CLIENT_ID}"
+              id="client_id"
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              placeholder="4fulp...4yoki"
+              aria-describedby="id-description"
+            />
+            <p class="mt-2 text-sm text-gray-500" id="id-description">
+              We will create a token with <code>chat:read</code> and
+              <code>chat:edit</code> scopes.
+            </p>
+            <button
+              type="button"
+              class="inline-flex items-center gap-x-1.5 rounded-md bg-[#6441a5] cursor-pointer px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-[#6441a5]/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Authorize on Twitch
+              <svg
+                class="-mr-0.5 size-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+                data-slot="icon"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </form>
+        </div>
       `,
     ),
   );
@@ -73,13 +116,13 @@ function webpage(title: string, body: string) {
         <title>${title.trim()}</title>
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <style type="text/tailwindcss">
-          @theme {
-            --color-clifford: #da373d;
-          }
+          @theme {}
         </style>
       </head>
-      <body>
-        ${body}
+      <body class="bg-gray-100 md:px-20 md:py-10 p-2">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8 rounded-lg">
+          ${body}
+        </div>
       </body>
     </html>
   `;
